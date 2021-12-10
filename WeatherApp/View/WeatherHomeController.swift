41b1c2239a2d18590a68eb363f29a77e098ad205
MiguelAngelSaravia <miguel.saravia@wizeline.com>
2021-12-10 12:08:52 -0400

    The default controller was deleted and the home weather was added

---
+++ WeatherApp/View/WeatherHomeController.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherHomeController.swift
+//  WeatherApp
+//
+//  Created by Miguel Saravia on 12/10/21.
+//
+
+import UIKit
+
+class WeatherHomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
